90427f43a807944f88c2cf16bf77f58885e8e21c
Zachary Juang <zachary822@me.com>
2023-06-13 00:37:28 -0400

    don't use edge

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({ adapter: netlify({ edge: true }) })],
+  plugins: [solid({ adapter: netlify() })],
   server: {
     strictPort: true,
     hmr: {
